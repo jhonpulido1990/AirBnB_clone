a26d61ef8ffc407f0d532cf7706fcd776597c2a3
jhonpulido1990 <3825@holbertonschool.com>
2022-03-01 15:30:27 -0500

    change in format of updated_at

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,10 @@ import uuid
 
 
 class BaseModel:
-    def __init__(self, created_at = datetime.now()):
+    def __init__(self):
         self.id = (uuid.uuid4()).hex
-        self.created_at = created_at
-        self.updated_at = created_at
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         '''
@@ -23,5 +23,5 @@ class BaseModel:
         dic = self.__dict__.copy()
         dic["__class__"] = self.__class__.__name__
         dic["created_at"] = self.created_at.isoformat()
-        dic["update"] = datetime.isoformat(datetime.now())
+        dic["updated_at"] = self.updated_at.isoformat()
         return(dic)

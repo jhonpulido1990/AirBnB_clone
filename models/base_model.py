dd36f2e8cbd7deccb50fdbba02fa150b027138c3
jhonpulido1990 <3825@holbertonschool.com>
2022-03-01 18:24:13 -0500

    add dict

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 from datetime import datetime
+from time import strftime
 import uuid
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = (uuid.uuid4()).hex
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs and len(kwargs) is not 0:
+            del kwargs['__class__']
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+            self.updated_at = datetime.strptime(self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+            self.created_at = datetime.strptime(self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+        if args is not None:
+            pass
 
     def __str__(self):
         '''
@@ -22,6 +32,5 @@ class BaseModel:
     def to_dict(self):
         dic = self.__dict__.copy()
         dic["__class__"] = self.__class__.__name__
-        dic["created_at"] = self.created_at.isoformat()
-        dic["updated_at"] = self.updated_at.isoformat()
+        dic.update({'created_at' : self.created_at.isoformat(), 'updated_at' : self.updated_at.isoformat()})
         return(dic)

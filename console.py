145b7ecca37ee4683b398cb9d85f14e18a9e4fb4
William05Cardozo <3784@holbertonschool.com>
2022-03-02 13:43:37 -0800

    Console0.0.1, All

--- console.py
+++ console.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 
 import cmd
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
     def do_quit(self, line):
+        'Programme output function'
         return exit(1)
+
+    def do_EOF(self, line):
+        'Programme output function'
+        return exit(1)
+
+    def do_all(self, line):
+        if self.__class__.__name__:
+            print("** class doesn't exist **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()

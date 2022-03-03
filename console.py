437ccf2bfe11b355586b035c8806490724b31cbe
William05Cardozo <3784@holbertonschool.com>
2022-03-02 16:29:14 -0800

    Console0.1, ShowCorrection

--- console.py
+++ console.py
@@ -25,5 +25,23 @@ class HBNBCommand(cmd.Cmd):
             print(Nline.id)
             Nline.save()
 
+        elif line != "BaseModel" and len(line) > 0:
+            print ("** class doesn't exist **")
+
+        else:
+            print("** class name missing **")
+
+    def do_show(self, line):
+        if len(line) == 0:
+           print("** class name missing **")
+        else:
+           line2 = line.split()
+           if line2[0] == "BaseModel" and len(line2) < 1:
+               print("** instance id missing **")
+           elif line2[0] != "BaseModel":
+               print("** class doesn't exist **")
+           else:
+               pass
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()

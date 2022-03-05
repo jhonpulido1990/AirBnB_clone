8441d93ac84d3caf713444ab9dd4ceac66983106
jhonpulido1990 <3825@holbertonschool.com>
2022-03-05 14:24:09 -0500

    test test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-
+'''impot module test'''
 import unittest
-
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
+
 
 class TestBaseModel(unittest.TestCase):
-
+    '''method by test'''
     @classmethod
     def setUpClass(cls):
+        '''funtion by instance'''
+
         cls.basemodel = BaseModel()
         cls.basemodel.firts_name = 'william'
         cls.basemodel.edad = '24'
@@ -16,14 +17,18 @@ class TestBaseModel(unittest.TestCase):
         cls.diccinary = BaseModel(**cls.basemodeldict)
 
     def test_BaseModel(self):
+        '''test'''
+
         self.assertEqual(self.basemodel.firts_name, 'william')
         self.assertTrue(self.basemodel.id)
         self.assertTrue(self.diccinary.created_at)
-        self.assertNotEqual(self.basemodel.created_at, self.diccinary.updated_at)
-        self.assertEqual(self.basemodel.firts_name, self.diccinary.firts_name)
-        self.assertEqual(type(self.basemodel.created_at), type(BaseModel().created_at))
+        self.assertNotEqual(self.basemodel.created_at,
+                            self.diccinary.updated_at)
+        self.assertEqual(self.basemodel.firts_name,
+                         self.diccinary.firts_name)
+        self.assertEqual(type(self.basemodel.created_at),
+                         type(BaseModel().created_at))
 
-    
 
 if __name__ == '__main__':
     unittest.main()

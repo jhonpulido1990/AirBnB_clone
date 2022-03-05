7618519fbcee12091b5dacc0f7294fbe39c6492c
jhonpulido1990 <3825@holbertonschool.com>
2022-03-05 10:36:27 -0500

    comment

--- models/state.py
+++ models/state.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""Module has:
-Imports from our archived base_model
-A class inherits BaseModel
-And a function init"""
+"""Module has: Imports from our archived base_model
+A class inherits BaseModel And a function init"""
 from models.base_model import BaseModel
-'''este es una descriccion del proyecto'''
 
 
 class State(BaseModel):
-    """ Class:
-    The class name is State and has a public atributte call "name"
+    """ Class: The class name is State and has a public atributte call "name"
     this class has a function init with two parameters *args and **kwargs
-    
+
     Args:
-        name (Str): The name of the state.
-    """
+        name (Str): The name of the state. """
     name = ""
 
     def __init__(self, *args, **kwargs):
-        """ Funciont:
-        This function receives two parameters *args(arguments)
+        """ Funciont: This function receives two parameters *args(arguments)
         and **kwargs(number of arguments)"""
         super().__init__(*args, **kwargs)

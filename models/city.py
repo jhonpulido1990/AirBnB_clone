6ce7acaf109ab0a397c132e4e373b6ce0b2ef3ec
jhonpulido1990 <3825@holbertonschool.com>
2022-03-05 11:20:11 -0500

    add comment 1

--- models/city.py
+++ models/city.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""
-Module has:
-Imports from our archived base_model
-A class inherits BaseModel
-And a function init
-"""
+"""Module has: Imports from our archived base_model
+A class inherits BaseModel And a function init"""
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """ Class:
-    The class name is City and has a publics atributtes calls state and name
-    this class has a function init with two parameters *args and **kwargs
-    """
+    """ Class: The class name is City and has a publics atributtes calls state and name
+    this class has a function init with two parameters *args and **kwargs"""
     state_id = ""
     name = ""
 
     def __init__(self, *args, **kwargs):
-        """ Funciont:
-        This function receives two parameters *args(arguments)
+        """ Funciont: This function receives two parameters *args(arguments)
         and **kwargs(number of arguments)"""
         super().__init__(*args, **kwargs)

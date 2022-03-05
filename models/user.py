425ca2761940001c3be81807f5a5c1269e36ea95
jhonpulido1990 <3825@holbertonschool.com>
2022-03-05 11:48:15 -0500

    add line

--- models/user.py
+++ models/user.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""
-Module has:
-Imports from our archived base_model
-A class inherits BaseModel
-And a function init
-"""
+"""Module has: Imports from our archived base_model
+A class inherits BaseModel And a function init"""
 
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ Class:
-    The class name is User and has a publics atributtes calls email, password,
-    first name and last name,this class has a function init with two
-    parameters *args and **kwargs
-    """
+    """ Class: The class name is User and has a publics atributtes
+    calls email, password, first name and last name,this class has a
+    function init with two parameters *args and **kwargs"""
+
     email = ""
     password = ""
     first_name = ""
@@ -22,4 +17,5 @@ class User(BaseModel):
 
     def __init__(self, *args, **kwargs):
         """Represents the User class with there arguments:"""
+
         super().__init__(*args, **kwargs)

1cb00ef40da7883226d6e07dce974653e879f0e1
William05Cardozo <3784@holbertonschool.com>
2022-03-05 13:01:41 -0800

    Correction in test-base-model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Imports modules testers"""
 
 import unittest
 import pep8
@@ -6,7 +7,9 @@ import os
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
+
 class TestBaseModel(unittest.TestCase):
+    """Testing"""
 
     @classmethod
     def setUpClass(cls):
@@ -17,19 +20,24 @@ class TestBaseModel(unittest.TestCase):
         cls.diccinary = BaseModel(**cls.basemodeldict)
 
     def test_BaseModel(self):
+        """Test of comprobation"""
         self.assertEqual(self.basemodel.firts_name, 'william')
         self.assertTrue(self.basemodel.id)
         self.assertTrue(self.diccinary.created_at)
-        self.assertNotEqual(self.basemodel.created_at, self.diccinary.updated_at)
+        self.assertNotEqual(self.basemodel.created_at,
+                            self.diccinary.updated_at)
         self.assertEqual(self.basemodel.firts_name, self.diccinary.firts_name)
 
     def test_pep8(self):
+        """Test of style"""
         st = pep8.StyleGuide(quiet=True)
         stx = st.check_files(['models/base_model.py'])
         self.assertEqual(stx.total_errors, 0, "check pep8")
 
     def test_docstring(self):
+        """Test of docstring"""
         self.assertTrue(len(self.basemodel.__doc__) > 0)
+
 
 if __name__ == '__main__':
     unittest.main()

1ac75285452a3480713fd7b56ffd36004660f5db
jhonpulido1990 <3825@holbertonschool.com>
2022-03-05 11:22:12 -0500

    space delect

--- models/city.py
+++ models/city.py
@@ -5,12 +5,13 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """ Class: The class name is City and has a publics atributtes calls state and name
+    """Class: The class name is City and has a publics atributtes calls state and name
     this class has a function init with two parameters *args and **kwargs"""
+
     state_id = ""
     name = ""
 
     def __init__(self, *args, **kwargs):
-        """ Funciont: This function receives two parameters *args(arguments)
+        """Funciont: This function receives two parameters *args(arguments)
         and **kwargs(number of arguments)"""
         super().__init__(*args, **kwargs)

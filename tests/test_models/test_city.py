246d83cbdf6bbf4d2fef34097f21aa9ce2775d0a
jhonpulido1990 <3825@holbertonschool.com>
2022-03-05 16:26:20 -0500

    add test files

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Imports modules testers"""
+
+import unittest
+import pep8
+import os
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class TestCity(unittest.TestCase):
+    """Testing"""
+
+    def test_pep8(self):
+        """Test of style"""
+        st = pep8.StyleGuide(quiet=True)
+        stx = st.check_files(['models/base_model.py'])
+        self.assertEqual(stx.total_errors, 0, "check pep8")
+
+    def test_docstring(self):
+        """Test of docstring"""
+        self.assertTrue(len(self.basemodel.__doc__) > 0)
+
+
+if __name__ == '__main__':
+    unittest.main()

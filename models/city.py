d6a435049fcdff924300250c66fb73ba3f88e6e2
jhonpulido1990 <3825@holbertonschool.com>
2022-03-05 11:31:58 -0500

    add line new

--- models/city.py
+++ models/city.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Module has: Imports from our archived base_model
 A class inherits BaseModel And a function init"""
+
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Class: The class name is City and has a publics atributtes calls state and name
-    this class has a function init with two parameters *args and **kwargs"""
+    """Class: The class name is City and has a publics atributtes
+    calls state and name this class has a function init with two
+    parameters *args and **kwargs"""
 
     state_id = ""
     name = ""

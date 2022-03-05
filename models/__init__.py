0265e0cd1c1a4b3e5428a67144af18bae41a3125
William05Cardozo <3784@holbertonschool.com>
2022-03-05 09:40:24 -0800

    Correction comments in Init

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""
-Module:
-Import our FileStorage from the archived file_store
-Instance create wiht name FileStora()
-storage call reload() function
-"""
+"""Module import our FileStorage from the archived file_store"""
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()

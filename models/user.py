77fbf4f9919c5b79633f710d1f96153bfb32549d
William05Cardozo <3784@holbertonschool.com>
2022-03-05 09:28:04 -0800

    Correction comments in User

--- models/user.py
+++ models/user.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
-"""
-Module has:
-Imports from our archived base_model
-A class inherits BaseModel
-And a function init
-"""
+"""Module has imports from our archived base_model"""
 
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ Class:
-    The class name is User and has a publics atributtes calls email, password,
-    first name and last name,this class has a function init with two
-    parameters *args and **kwargs
-    """
+    """ Class user"""
+
     email = ""
     password = ""
     first_name = ""
@@ -22,4 +14,5 @@ class User(BaseModel):
 
     def __init__(self, *args, **kwargs):
         """Represents the User class with there arguments:"""
+
         super().__init__(*args, **kwargs)

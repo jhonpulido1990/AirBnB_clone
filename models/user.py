bc07c219c188c1d2be09d979562bc26cbf5d6b29
jhonpulido1990 <3825@holbertonschool.com>
2022-03-05 10:41:49 -0500

    add commenet atribute

--- models/user.py
+++ models/user.py
@@ -13,18 +13,18 @@ class User(BaseModel):
     """ Class:
     The class name is User and has a publics atributtes calls email, password,
     first name and last name,this class has a function init with two
-    parameters *args and **kwargs"""
+    parameters *args and **kwargs
+        Args:
+            email (str): The email of the user.
+            password (str): The password of the user.
+            firstname (str): The first name of the user.
+            last_name (str): The last name of the user.
+    """
     email = ""
     password = ""
     first_name = ""
     last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """Represents the User class with there arguments:
-        Args:
-            email (str): The email of the user.
-            password (str): The password of the user.
-            firstname (str): The first name of the user.
-            last_name (str): The last name of the user.
-        """
+        """Represents the User class with there arguments:"""
         super().__init__(*args, **kwargs)

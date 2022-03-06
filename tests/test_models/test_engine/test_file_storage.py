424da92af2f5e4c4c779e7b89d9bcd0ef08d47c7
William05Cardozo <3784@holbertonschool.com>
2022-03-05 21:58:05 -0800

    Correction test-file-storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Imports modules for testing"""
-import pycodestyle
+import pep8
 import unittest
 import json
 import os
@@ -19,19 +19,26 @@ class TestFileStorage(unittest.TestCase):
         cls.filestorage = FileStorage()
         cls.filestorage.firts_name = 'william'
         cls.filestorage.edad = '24'
-        cls.filestoragedict = cls.filestorage.to_dict()
-        cls.diccinary = FileStorage(**cls.filestoragedict)
 
-    def test_pycodestyle(self):
-        """Test of pycodestyle"""
-        st = pycodestyle.StyleGuide(quiet=True)
+    def test_BaseModel(self):
+        """Test of comprobation"""
+        self.assertEqual(self.filestorage.firts_name, 'william')
+        self.assertTrue(self.filestorage.id)
+        self.assertTrue(self.diccinary.created_at)
+        self.assertNotEqual(self.filestorage.created_at,
+                            self.diccinary.updated_at)
+        self.assertEqual(self.filestorage.firts_name, self.diccinary.firts_name)
+
+    def test_pep8(self):
+        """Test of pep8"""
+        st = pep8.StyleGuide(quiet=True)
         r = st.check_files(['models/engine/file_storage.py'])
         self.assertEqual(r.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_Documentation(self):
         """Test of documentation"""
-        self.assertTrue(len(models.engine.file_storage.__doc__) > 0)
+        self.assertTrue(len(self.filestorage__doc__) > 0)
 
 
 if __name__ == '__main__':

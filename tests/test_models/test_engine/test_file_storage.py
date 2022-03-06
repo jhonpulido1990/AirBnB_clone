7033301376dca52a4c1a3b76412d8066a908ea95
jhonpulido1990 <3825@holbertonschool.com>
2022-03-06 01:21:12 -0500

    add macro

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Imports modules for testing"""
+import pep8
 import unittest
+import json
 from models.engine.file_storage import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
@@ -17,3 +19,14 @@ class TestFileStorage(unittest.TestCase):
             this test validate that creation proccess was correct.
         '''
         self.assertEqual(self.storage.save(), None)
+
+    def test_pep8(self):
+        """Test of pep8"""
+        st = pep8.StyleGuide(quiet=True)
+        r = st.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(r.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+
+if __name__ == '__main__':
+    unittest.main()

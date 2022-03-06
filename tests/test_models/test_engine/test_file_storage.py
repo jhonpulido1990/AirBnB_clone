2fb855f21ccb78320f6161e2e2921b2761c373d6
jhonpulido1990 <3825@holbertonschool.com>
2022-03-06 16:09:13 -0500

    add test filestorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,9 +3,10 @@
 import pep8
 import unittest
 import json
-from models.engine.file_storage import BaseModel
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
+from models.user import User
 
 
 class TestFileStorage(unittest.TestCase):
@@ -13,6 +14,7 @@ class TestFileStorage(unittest.TestCase):
 
     def setUp(self):
         self.storage = FileStorage()
+        self.usuario = User()
 
     def test_creation(self):
         '''
@@ -26,6 +28,16 @@ class TestFileStorage(unittest.TestCase):
         r = st.check_files(['models/engine/file_storage.py'])
         self.assertEqual(r.total_errors, 0,
                          "Found code style errors (and warnings).")
+    
+    def test_User_saveStorage(self):
+        """ Checks if the save function works """
+        self.usuario.first_name = "betty"
+        self.usuario.save()
+        self.storage.reload()
+        self.storage.all()
+        self.assertIsInstance(self.storage.all(), dict)
+        self.assertTrue(hasattr(self.usuario, 'save'))
+        self.assertNotEqual(self.usuario.created_at, self.usuario.updated_at)
 
 
 if __name__ == '__main__':

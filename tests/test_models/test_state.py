cb8a75c5058543e6746bb3604a782d3d5bc2ba5e
William05Cardozo <3784@holbertonschool.com>
2022-03-06 20:57:01 -0800

    Correction Test_state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -67,6 +67,35 @@ class TestState(unittest.TestCase):
         """ Test to_dict method inherited from BaseModel """
         self.assertEqual('to_dict' in dir(self.statemodel), True)
 
+    def setUp(self):
+        """Setting up"""
+        self.obj_state = State()
+
+    def tearDown(self):
+        """Cleaning up after each test"""
+        del self.obj_state
+
+    def test_is_instance(self):
+        """Check if an instance belongs to class State"""
+        self.assertIsInstance(self.obj_state, State)
+
+    def test_if_str(self):
+        """Check if the attribute is str"""
+        self.assertIsInstance(self.obj_state.name, str)
+
+    def test_is_an_instance(self):
+        """Check if my_model is an instance of BaseModel"""
+        self.assertIsInstance(self.obj_state, State)
+
+    def test_module_doc(self):
+        """ Method to check for module documentation."""
+        self.assertTrue(len(self.obj_state.__doc__) > 0)
+
+    def test_method_docs(self):
+        """ Method to check for method´s documentation."""
+        for func in dir(State):
+            self.assertTrue(len(func.__doc__) > 0)
+
 
 if __name__ == '__main__':
     unittest.main()
